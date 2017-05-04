6d7780859b0ff633e779603805b1b309c2d7049b
Dave Van Vessem <dave@undarl.com>
2017-05-04 17:22:43 -0400

    Tweak fusion generator script, add fusion calc doc

--- fusion-generator-scripts.lua
+++ fusion-generator-scripts.lua
@@ -1,3 +1,6 @@
+--Fusion generator temperature setting and update logic borrowed
+--with thanks from Klonan's diesel generator.
+
 function migrate_fusion_generators(surface)
 	if global.fusion_gens then return end
 	global.fusion_generator = nil
@@ -29,6 +32,8 @@ function check_fusion_generators()
 		if (k + tick) % interval == 0 then
 			if not gen.valid then
 				gens[k] = nil
+				global.fusion_gens_size = #gens
+				global.fusion_gens_interval = math.ceil(global.fusion_gens_size / 200)
 			else
 				heat(gen)
 			end
