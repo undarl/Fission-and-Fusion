2d6f56cd37c4f50ec39a61cd080e07d2cc7538ea
Dave Van Vessem <dave@undarl.com>
2017-05-18 16:31:35 -0400

    Fix migration file

--- migrations/Fission and Fusion_0.3.0.lua
+++ migrations/Fission and Fusion_0.3.0.lua
@@ -1,4 +1,10 @@
 for _, force in pairs(game.forces) do
- force.reset_recipes()
- force.reset_technologies()
+    force.reset_recipes()
+    force.reset_technologies()
+    if force.technologies["undarl-radioisotopes"].researched then
+        force.recipes["undarl-fast-rtg-equipment"].enabled = true
+        if settings.startup['undarl-fast-rtg-recycling'].value ~= "none" then
+            force.recipes["undarl-fast-rtg-recycling"].enabled = true
+        end
+    end
 end
